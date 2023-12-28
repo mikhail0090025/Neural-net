4b058eb8daaf1bd2fa95491c55173e0b39238f9f
mikhail0090025 <52649381+mikhail0090025@users.noreply.github.com>
2023-12-28 21:02:56 +0100

    Working creation of generation, updated UI
    Bugs with creation generation was fixed, UI was updated

--- NeuralNetWebASP/Controllers/NeuralNetsController.cs
+++ NeuralNetWebASP/Controllers/NeuralNetsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Neural_net;
 using NeuralNetWebASP.Models;
 
@@ -7,27 +8,39 @@ namespace NeuralNetWebASP.Controllers
     [Route("api/[controller]")]
     public class NeuralNetsController : Controller
     {
-        Generation Generation { get; set; }
-        LearningDatabase LearningDatabase { get; set; }
+        static Generation Generation { get; set; }
+        static LearningDatabase LearningDatabase { get; set; }
+        ILogger<NeuralNetsController> Logger { get; set; }
+        public NeuralNetsController(ILogger<NeuralNetsController> _logger)
+        {
+            Logger = _logger;
+            //Generation = new Generation(5, 5, 5, 5, 30, RoundType.DontRound, RoundType.ZeroAndOne, RoundType.Tanh, 0.0001);
+            //LearningDatabase = new LearningDatabase(5, 5);
+            //Generation.SetDatabase(LearningDatabase);
+        }
         public IActionResult Index()
         {
             return View();
         }
-        [HttpPost("NewGeneration")]
+        [HttpPost("newgeneration")]
         public IActionResult CreateNewGeneration([FromForm] GenerationParameters parameters)
         {
-            try
-            {
+            Logger.LogInformation(Json(parameters).ToString());
+            Logger.LogInformation("Creating new generation started!");
+			try
+			{
                 parameters.learning_factor = 1 / parameters.learning_factor;
                 Generation = new Generation(parameters.inputs_count, parameters.outputs_count, parameters.hidden_layers_count, parameters.neurals_in_hidden_layer_count, parameters.gen_size, (RoundType)parameters.inp_round, (RoundType)parameters.neu_round, (RoundType)parameters.out_round, parameters.learning_factor);
                 LearningDatabase = new LearningDatabase(parameters.inputs_count, parameters.outputs_count);
                 Generation.SetDatabase(LearningDatabase);
-            }
+                Logger.LogInformation("I am here");
+			}
             catch(Exception ex)
             {
                 return Json(new { text = $"Error: {ex.Message}"});
             }
-            return Json(new { text = $"Neural net population was successfully created! Generation is null: {Generation == null}" });
+            Logger.LogInformation("End of creating generation");
+            return Json(new { text = $"Generation was created!" });
         }
         [HttpPost("AddToDB")]
         public IActionResult AddElementToDB([FromBody] object data)
@@ -37,6 +50,10 @@ namespace NeuralNetWebASP.Controllers
         }
         private GenerationParameters GenParams()
         {
+            if(Generation == null)
+            {
+                throw new Exception("Generation is null");
+            }
 			GenerationParameters parameters = new GenerationParameters();
 			parameters.inputs_count = Generation.InputsCount;
 			parameters.outputs_count = Generation.OutputsCount;
@@ -57,12 +74,21 @@ namespace NeuralNetWebASP.Controllers
         [HttpGet("GetAddingToDBview")]
         public IActionResult GetAddingToDBview()
         {
+            Logger.LogInformation("Request to 'GetAddingToDBview' has come!");
             if (Generation == null)
             {
+                Logger.LogInformation("111");
                 return BadRequest("Generation was not created!");
             }
-            var parameters = GenParams();
+			var parameters = GenParams();
+			Logger.LogInformation("111");
             return View("AddToDBView", parameters);
+        }
+        [HttpGet("generationinjson")]
+        public IActionResult GenerationInJson()
+        {
+            //return Json(Generation);
+            return Json(new { gen = Generation, isnull = (Generation == null)});
         }
 	}
 }
